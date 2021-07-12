23550345f59e1f04641009605a637fbecca26697
Samaksh Gupta <samaksh@tsbmediaventure.com>
2021-07-12 20:15:49 +0530

    config

--- support/configuration.js
+++ support/configuration.js
@@ -30,8 +30,6 @@ module.exports = {
       address: ['address'],
       email: ['email', 'email_verified'],
       phone: ['phone_number', 'phone_number_verified'],
-      profile: ['birthdate', 'family_name', 'gender', 'given_name', 'locale', 'middle_name', 'name',
-        'nickname', 'picture', 'preferred_username', 'profile', 'updated_at', 'website', 'zoneinfo'],
     },
     features: {
       devInteractions: { enabled: false }, // defaults to true
